a556fee5994e2eec8bb6b7ea7cac240e5ed3d655
sakshamkadayat <sakshamkadayat@gmail.com>
2023-06-10 11:25:52 +0545

    login and register done

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,14 +1,11 @@
 import "../styles/Login.css";
-import {Link } from "react-router-dom";
-import Goverment from "../../images/goverment_logo.png";
-import {useRef} from "react";
-import namaste from "../../images/namaste.png";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../context/ContextProvider";
 
-
 const Login = () => {
-    const {settingUser,settingToken} = useStateContext();
+    const { settingUser, settingToken } = useStateContext();
     const emailRef = useRef();
     const passwordRef = useRef();
     const handleSubmit = (e) => {
@@ -17,61 +14,54 @@ const Login = () => {
             email: emailRef.current.value,
             password: passwordRef.current.value,
         };
-      
-        axiosClient.post("/login", payLoad)
+
+        axiosClient
+            .post("/login", payLoad)
             .then((response) => {
                 settingUser(response.data.user);
                 settingToken(response.data.access_token);
-            }
-            )
+            })
             .catch((error) => {
                 console.log(error);
-            }
-            );
+            });
     };
-
-
 
     return (
         <div className="login-maincontainer">
             <div className="login-leftcontainer">
-                <h3 className="global-font">
-                    Introducing <br /> New rules and regulations{" "}
-                </h3>
-                <h2 className="global-font"> For a better tomorrow</h2>
-                <img src={namaste} alt="flag" />
-                <h2 className="global-font">
-                    {" "}
-                    Stay informed for a brighter future.
-                </h2>
-            </div>
-            <div className="login-rightcontainer">
+                <h3 className="global-font">WELCOME BACK !</h3>
                 <form action="#" onSubmit={handleSubmit} method="post">
                     <div className="form-field">
-                        <img
-                            src={Goverment}
-                            alt="images"
-                            className="login-left-image"
+                        <input
+                            ref={emailRef}
+                            type="email"
+                            required
+                            name="email"
+                            id="email"
+                            placeholder="Email Address"
                         />
-                        <h3 style={{ color: "blue" }} className="global-font">
-                            GOVERMENT{" "}
-                            <span style={{ color: "red" }}>OF NEPAL</span>
-                        </h3>
                     </div>
                     <div className="form-field">
-                        <label htmlFor="email">Email </label>
-                        <input ref={emailRef} type="email" required name="email" id="email" />
+                        <input
+                            ref={passwordRef}
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Password"
+                        />
                     </div>
                     <div className="form-field">
-                        <label htmlFor="password">Password </label>
-                        <input ref={passwordRef} type="password" name="password" id="password" />
-                    </div>
-                    <div className="button">
-                        <input type="submit" value="Login" />
+                        <a href="#" className="forgot global-font">
+                            Forgot password ?
+                        </a>
                     </div>
                     <div className="form-field">
-                        <p>
-                            Not Registered yet ?  <Link to="/guest/register">Register</Link>
+                        <input type="submit" value="Login" className="btn" />
+                    </div>
+                    <div className="form-field">
+                        <p className="global-font">
+                            Don't have an account ?{" "}
+                            <Link to="/guest/register" style={{color:'red'}} className="global-font">Signup</Link>
                         </p>
                     </div>
                 </form>
